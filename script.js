8922df5222251c2855ca4e182ffa36a1bedcf2fc
MOHAMMED-IYAD <mohammedprograme@gmail.com>
2026-06-02 01:25:58 +0300

    update website

--- script.js
+++ script.js
@@ -115,4 +115,26 @@ Sent from Portfolio Website`;
 
   window.open(gmailUrl, "_blank");
 };
+
+
+
+
+});
+document.addEventListener("DOMContentLoaded", function () {
+
+  let index = 0;
+  const slides = document.querySelectorAll(".slide");
+
+  function showSlide() {
+    slides.forEach(s => s.classList.remove("active"));
+
+    slides[index].classList.add("active");
+
+    index++;
+    if (index >= slides.length) index = 0;
+  }
+
+  showSlide();
+  setInterval(showSlide, 3500);
+
 });
